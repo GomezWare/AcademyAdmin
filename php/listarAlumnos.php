3f8e49f93b9eee8b89ea5f65625edc610f052138
GomezWare <streetninjacoder@gmail.com>
2024-02-19 09:29:37 +0100

    Creado script para listar Almunos

--- php/listarAlumnos.php
+++ php/listarAlumnos.php
@@ -4,4 +4,24 @@
 // Conexion DB
 $db = new PDO('sqlite:' . '../basedatos/bd.sqlite');
 
-$query = "SELECT * FROM student";
+// Preparacion de la query
+$query = "SELECT * FROM students";
+$stmt = $db->query($query);
+
+// Ejecucucion de la consulta
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Este es el array que se devolvera como un JSON
+$json = array();
+ 
+// Se vuelcan los datos en forma de array asociativo
+foreach ($rows as $row) {
+    $json[] = $row;
+}
+
+// Se codifica el array en forma de JSON y se establecen las cabeceras de la peticion
+$response = json_encode($json);
+header('Content-Type: application/json');
+
+// Se responde con el JSON
+echo $response;
