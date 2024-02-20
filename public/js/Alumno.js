bcb0128a460ccb461171a765461a8ee3e6bdf83b
GomezWare <streetninjacoder@gmail.com>
2024-02-20 12:57:33 +0100

    Opciones en la tabla

--- public/js/Alumno.js
+++ public/js/Alumno.js
@@ -15,12 +15,16 @@ class Alumno {
     let idTd = document.createElement("td");
     let nameTd = document.createElement("td");
     let telTd = document.createElement("td");
+    let optTd = document.createElement("td");
+    optTd.innerHTML = `
+    <button class="detalles">Detalles</button><button class="borrar">Borrar</button><button class="modificar">Modificar</button><button class="calificar">Calificar</button>`;
     idTd.innerText = this.id;
     nameTd.innerText = this.name;
     telTd.innerText = this.tel;
     row.appendChild(idTd);
     row.appendChild(nameTd);
     row.appendChild(telTd);
+    row.appendChild(optTd);
     return row;
   }
 }
