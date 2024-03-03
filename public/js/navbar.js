12ac630cbb601231c96d73bedecd700ac10d2cb4
GomezWare <streetninjacoder@gmail.com>
2024-03-03 20:01:59 +0100

    Proyecto finalizado

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -35,3 +35,13 @@ document.querySelector("#aInicio").addEventListener("click", () => {
   let dominio = window.location.protocol + "//" + window.location.host;
   window.location.href = dominio + "/index.html";
 });
+
+// Evento por si se pulsa el boton github
+document.querySelector("#aGithub").addEventListener("click", () => {
+  window.location.href = "https://github.com/GomezWare/AcademyAdmin";
+});
+
+// Evento por si se pulsa el boton github movil
+document.querySelector("#aGithubM").addEventListener("click", () => {
+  window.location.href = "https://github.com/GomezWare/AcademyAdmin";
+});
